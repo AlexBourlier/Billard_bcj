09f128ad79a3c6fe5636d5437a52f38aafd271be
Alexdessine <alexandre@bourlier.email>
2026-04-22 16:37:14 +0200

    Ajout des données dans la migration calendar_event_links_table

--- database/migrations/2026_04_22_163401_create_calendar_event_links_table.php
+++ database/migrations/2026_04_22_163401_create_calendar_event_links_table.php
@@ -6,20 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('calendar_event_links', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('calendar_event_id')
+                ->constrained('calendar_events')
+                ->cascadeOnDelete();
+
+            $table->string('category', 100);
+            $table->string('label')->nullable();
+            $table->text('url');
+            $table->integer('sort_order')->nullable();
+
             $table->timestamps();
+
+            $table->index('calendar_event_id', 'calendar_event_links_calendar_event_id_index');
+            $table->index('category', 'calendar_event_links_category_index');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('calendar_event_links');
