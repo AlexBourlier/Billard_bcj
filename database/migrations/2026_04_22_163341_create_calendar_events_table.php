bf0c74b07cb52573dc7f7270793ffdc70d0e2cba
Alexdessine <alexandre@bourlier.email>
2026-04-22 16:36:28 +0200

    Ajout des informations dans la migration calendar_events_table

--- database/migrations/2026_04_22_163341_create_calendar_events_table.php
+++ database/migrations/2026_04_22_163341_create_calendar_events_table.php
@@ -6,20 +6,40 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('calendar_events', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('calendar_id')
+                ->constrained('calendars')
+                ->cascadeOnDelete();
+
+            $table->string('external_id')->nullable();
+
+            $table->dateTime('date_debut');
+            $table->dateTime('date_fin');
+            $table->dateTime('date_limite')->nullable();
+
+            $table->string('titre');
+            $table->string('lieu');
+            $table->string('club')->nullable();
+
+            $table->text('url')->nullable();
+            $table->string('status', 100)->nullable();
+
+            $table->json('source_payload')->nullable();
+
             $table->timestamps();
+
+            $table->index('calendar_id', 'calendar_events_calendar_id_index');
+            $table->index('date_debut', 'calendar_events_date_debut_index');
+            $table->index(['calendar_id', 'date_debut'], 'calendar_events_calendar_id_date_debut_index');
+
+            $table->unique(['calendar_id', 'external_id'], 'calendar_events_calendar_id_external_id_unique');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('calendar_events');
