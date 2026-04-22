d38b79da6fc3b8aa84b550efa0d5818e41dd2f50
Alexdessine <alexandre@bourlier.email>
2026-04-22 16:35:29 +0200

    Ajout des données dans la migration create calendars table

--- database/migrations/2026_04_22_163322_create_calendars_table.php
+++ database/migrations/2026_04_22_163322_create_calendars_table.php
@@ -6,20 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
+
+            $table->string('discipline', 50);
+            $table->string('scope', 50);
+            $table->string('name')->nullable();
+            $table->string('slug')->unique();
+            $table->string('source_type', 50)->nullable();
+
+            $table->boolean('is_active')->default(true);
+
             $table->timestamps();
+
+            $table->unique(['discipline', 'scope'], 'calendars_discipline_scope_unique');
+            $table->index('is_active', 'calendars_is_active_index');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('calendars');
