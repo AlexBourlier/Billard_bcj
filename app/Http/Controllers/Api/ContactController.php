17fd83e2245d9735b53a2d9183a7a45310c84486
Alexdessine <alexandre@bourlier.email>
2026-04-25 13:33:23 +0200

    Ajout des commentaires sur api/ContactController

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -7,8 +7,21 @@ use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\JsonResponse;
 
+/**
+ * Contrôleur API pour la gestion des contacts.
+ *
+ * Expose les informations de contact publiques du club.
+ *
+ * Les réponses respectent le format standard :
+ * data / meta / links / error
+ */
 class ContactController extends Controller
 {
+    /**
+     * Retourne la liste des contacts disponibles.
+     *
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         $contact = Contact::all();
