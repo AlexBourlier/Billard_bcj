43e98e3d5dc4a563bee896156e2b741d0f033077
Alexdessine <alexandre@bourlier.email>
2026-05-06 12:10:36 +0200

    Mise en place du css pour le footer

---
+++ frontend/src/components/layout/MobileFooter.tsx
@@ -0,0 +1,77 @@
+import { NavLink } from "react-router-dom";
+import type { Menu } from "../../types/api";
+import { getMenuPath } from "./navigationUtils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+    faFacebook,
+} from "@awesome.me/kit-c0df283285/icons/classic/brands";
+
+import {
+    faEnvelope,
+    faLocationDot,
+} from "@awesome.me/kit-c0df283285/icons/classic/solid";
+
+type Props = {
+    logoUrl: string | null;
+};
+
+export function MobileFooter({ logoUrl }: Props) {
+    return (
+        <div className="mobile-footer-wrapper">
+            <div className="container mobile-footer">
+                <h4 className="site-logo-mobile">
+                    <NavLink end to="/">
+                        {logoUrl ? (
+                            <img
+                                src={logoUrl}
+                                alt="BCJ37 - Billard Club de Joué-lès-Tours"
+                                className="site-logo__image"
+                            />
+                        ) : (
+                            <span>BCJ37</span>
+                        )}
+                    </NavLink>
+                </h4>
+
+                <div className="social-links-mobile">
+                    <p className="contact-info">Contact</p>
+                    <a
+                        href="https://www.facebook.com/people/Billard-Club-de-Joué-Lès-Tours/61573797213739/?locale=fr_FR"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faFacebook} aria-hidden="true" />
+                        <span className="sr-only">Facebook</span>
+                        Billard Club de Joué-lès-Tours
+                    </a>
+
+                    <NavLink to="/contact" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" />
+                        <span className="sr-only">Contact</span>
+                        contact@bcj37.fr
+                    </NavLink>
+
+                    <address className="contact-address">
+                        <FontAwesomeIcon icon={faLocationDot} aria-hidden="true" />
+                        <span className="sr-only">Adresse</span>
+                        28 Rue Joseph Cugnot, 37300 Joué-Lès-Tours
+                    </address>
+
+                    <p className="footer-note">Affilié à la <a
+                        href="https://www.ffbillard.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <span className="sr-only">Fédération Française de Billard</span>
+                        Fédération Française de Billard
+                    </a></p>
+                </div>
+
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; {new Date().getFullYear()} BCJ37 - Billard Club de Joué-lès-Tours. Tous droits réservés.</p>
+            </div>
+        </div>
+    );
+}
