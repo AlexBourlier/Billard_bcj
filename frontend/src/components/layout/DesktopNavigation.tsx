04914e4cca889f11e6b0bcd17fe119c9b670bfa8
Alexdessine <alexandre@bourlier.email>
2026-05-06 10:54:09 +0200

    Implémentation du header responsive

--- frontend/src/components/layout/DesktopNavigation.tsx
+++ frontend/src/components/layout/DesktopNavigation.tsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import type { Menu } from "../../types/api";
 import { getMenuPath } from "./navigationUtils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+    faFacebook,
+    faYoutube,
+} from "@awesome.me/kit-c0df283285/icons/classic/brands";
+
+import { 
+    faCalendarAlt, 
+    faEnvelope, 
+} from "@awesome.me/kit-c0df283285/icons/classic/solid";
 
 type Props = {
     menus: Menu[];
@@ -11,8 +22,8 @@ export function DesktopNavigation({ menus, logoUrl }: Props) {
     return (
         <div className="desktop-nav-wrapper">
             <div className="container desktop-nav">
-                <h1 className="site-logo">
-                    <Link to="/">
+                <h1 className="site-logo-desktop">
+                    <NavLink end to="/">
                         {logoUrl ? (
                             <img
                                 src={logoUrl}
@@ -22,16 +33,59 @@ export function DesktopNavigation({ menus, logoUrl }: Props) {
                         ) : (
                             <span>BCJ37</span>
                         )}
-                    </Link>
+                    </NavLink>
                 </h1>
 
-                <nav aria-label="Navigation principale desktop">
+                <nav aria-label="navigation-principale-desktop">
                     {menus.map((menu) => (
-                        <Link key={menu.id} to={getMenuPath(menu.name)}>
-                            {menu.name}{' '}
-                        </Link>
+                        <NavLink
+                            key={menu.id}
+                            to={getMenuPath(menu.name)}
+                            className={({ isActive }) =>
+                                isActive ? "is-active" : ""
+                            }
+                        >
+                            {menu.image_url ? (
+                                <img
+                                    src={menu.image_url}
+                                    alt={menu.name}
+                                    className="menu-image"
+                                />
+                            ) : (
+                                menu.name
+                            )}
+                        </NavLink>
                     ))}
                 </nav>
+                <div className="social-links">
+                    <NavLink to="/calendrier" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faCalendarAlt} aria-hidden="true" />
+                        <span className="sr-only">Calendrier</span>
+                    </NavLink>
+
+                    <a
+                        href="https://www.facebook.com/people/Billard-Club-de-Joué-Lès-Tours/61573797213739/?locale=fr_FR"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon icon={faFacebook} aria-hidden="true" />
+                        <span className="sr-only">Facebook</span>
+                    </a>
+
+                    <a 
+                        href="https://www.youtube.com/@BCJ37" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+
+                        <FontAwesomeIcon icon={faYoutube} aria-hidden="true" />
+                        <span className="sr-only">YouTube</span>
+                    </a>
+
+                    <NavLink to="/contact" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faEnvelope} aria-hidden="true" />
+                        <span className="sr-only">Contact</span>
+                    </NavLink>
+                </div>
             </div>
         </div>
     );
